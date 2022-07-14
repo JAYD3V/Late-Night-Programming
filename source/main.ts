5d9c191703b10d70c35f401c2cad845127b2dd96
JAYD3V <W3Dojo@Gmail.com>
2022-07-14 03:41:31 -0700

    Added TypeScript to project

---
+++ source/main.ts
@@ -0,0 +1,5 @@
+import vscode from 'vscode';
+import nocturnal from './json/nocturnal-clr-scheme.json' assert { type: 'json' };
+
+console.log('Hello World!\n\n');
+console.log(nocturnal.CONFIGURABLE_GROUP_ALPHA.properties);
