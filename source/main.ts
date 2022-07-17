35d486a9b51a52bec35faf0b5de87f5b272d739e
JAYD3V <W3Dojo@Gmail.com>
2022-07-17 12:06:40 -0700

    refactor(./theme/nocturnal-vscode-theme.json) Adjusted textMateTokens in the nocturnal theme.

--- source/main.ts
+++
@@ -1,5 +0,0 @@
-import vscode from 'vscode';
-import nocturnal from './json/nocturnal-clr-scheme.json' assert { type: 'json' };
-
-console.log('Hello World!\n\n');
-console.log(nocturnal.CONFIGURABLE_GROUP_ALPHA.properties);
